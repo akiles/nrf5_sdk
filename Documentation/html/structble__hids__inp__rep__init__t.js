58464c77049f81454c7fd9b82228b417c0c1a15c
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-09-16 17:51:32 +0200

    nrf51_sdk_v4_4_2_33551.zip

---
+++ Documentation/html/structble__hids__inp__rep__init__t.js
@@ -0,0 +1,6 @@
+var structble__hids__inp__rep__init__t =
+[
+    [ "max_len", "structble__hids__inp__rep__init__t.html#a1a6c02276292f164788906e8d5a316e5", null ],
+    [ "rep_ref", "structble__hids__inp__rep__init__t.html#a070c6ac5d77d97faf98946507b8f8b7b", null ],
+    [ "security_mode", "structble__hids__inp__rep__init__t.html#a5cd8f181c0b16319634a316726501c64", null ]
+];
